ae0d55f60ca8ef10e5556a9dcb7d9264b27b669b
Narihito <heinboss234@gmail.com>
2025-11-17 19:00:40 +0630

    f

--- controllers/usercontroller.js
+++ controllers/usercontroller.js
@@ -4,7 +4,7 @@ import User from "../models/userquery.js";
 export async function getuser(req, res) {
     await mongoconnect();
     try {
-        const { username, email, text } = req.body;
+        const { user, email, text } = req.body;
         await User.create({ name: username, email: email, text: text });
         return res.status(200).json({ success: true, message: "Sent Successfully!" });
     }
