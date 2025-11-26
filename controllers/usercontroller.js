ac4b0c15e1f16ce92b6bc9cbef115965049c182a
Narihito <heinboss234@gmail.com>
2025-11-26 20:25:28 +0630

    added fetch

--- controllers/usercontroller.js
+++ controllers/usercontroller.js
@@ -1,3 +1,4 @@
+
 import { mongoconnect } from "../db/dbconnect.js";
 import User from "../models/userquery.js";
 
@@ -12,4 +13,16 @@ export async function getuser(req, res) {
     {
         return res.status(200).json({success: false,message: "Unexpected Error"});
     }
+}
+
+export async function fetchuser(req,res){
+   await mongoconnect();
+   try{
+     const fetchuser = await User.find({});
+     return res.status(200).json({success: true, fetchuser});
+   }
+   catch(err)
+   {
+    return res.status(500).json({success: false, message:"Unexpected Error"});
+   }
 }
--- routes/route.js
+++ routes/route.js
@@ -1,4 +1,4 @@
-import { getuser } from "../controllers/usercontroller.js";
+import { fetchuser, getuser } from "../controllers/usercontroller.js";
 import cors from "cors";
 import express from "express";
 import { Router } from "express";
@@ -7,7 +7,7 @@ const router = express.Router();
 
 
 router.post("/api/senduser", getuser);
-
+router.get("/api/fetchuser",fetchuser);
 export default router;
 
 
